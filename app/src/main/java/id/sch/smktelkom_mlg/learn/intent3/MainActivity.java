8f556c51f8ee7aef38d0d2c054e0c8ce70d63ef1
Vergie Ericson T <vergieericsont@gmail.com>
2016-11-08 04:39:43 +0700

    Intent Telepon

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,13 +1,35 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageViewPhone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+dialPhoneNumber("0341712500");
+            }
+        });
+    }
+
+    public void dialPhoneNumber(String phoneNumber){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:"+phoneNumber));
+        if(intent.resolveActivity(getPackageManager()) !=null)
+            startActivity(intent);
     }
 }
